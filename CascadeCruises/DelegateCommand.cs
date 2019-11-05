ff5315333243e8d8b5f781502f66d9a6e908849f
Kevork <kevork_t@abv.bg>
2019-11-05 07:17:31 +0200

    Добавен основен viewmodel и показване на някои от таблиците.

---
+++ CascadeCruises/DelegateCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CascadeCruises
+{
+    public class DelegateCommand : ICommand
+    {
+
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameters)
+        {
+            return _canExecute == null ? true : _canExecute(parameters);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameters)
+        {
+            _execute(parameters);
+        }
+    }
+}
---
+++ CascadeCruises/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CascadeCruises.ViewModels
+{
+   public class MainViewModel
+    {
+        private Cascade_CruisesEntities _dbContext;
+        public ObservableCollection<Ship> ShipRecords { get; set; }
+        public ObservableCollection<Cruise> CruiseRecords { get; set; }
+
+        public MainViewModel()
+        {
+            _dbContext = new Cascade_CruisesEntities();
+            ShipRecords = new ObservableCollection<Ship>(_dbContext.Ships);
+            CruiseRecords = new ObservableCollection<Cruise>(_dbContext.Cruises);
+        }  
+    }
+}
