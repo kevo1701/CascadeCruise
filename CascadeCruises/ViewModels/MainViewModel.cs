3cc7efc8696da9e2d4fe0700c33632ac6181589d
Kevork <kevork_t@abv.bg>
2019-11-06 02:18:21 +0200

    Преглед на таблиците от базата

--- CascadeCruises/ViewModels/MainViewModel.cs
+++ CascadeCruises/ViewModels/MainViewModel.cs
@@ -14,12 +14,19 @@ namespace CascadeCruises.ViewModels
         private Cascade_CruisesEntities _dbContext;
         public ObservableCollection<Ship> ShipRecords { get; set; }
         public ObservableCollection<Cruise> CruiseRecords { get; set; }
-
+        public ObservableCollection<Room> RoomRecords { get; set; }
+        public ObservableCollection<Passenger> PassengerRecords { get; set; }
+        public ObservableCollection<Destination> DestinationRecords { get; set; }
+        public ObservableCollection<Cruise_Duration> Cruise_DurationRecords { get; set; }
         public MainViewModel()
         {
             _dbContext = new Cascade_CruisesEntities();
             ShipRecords = new ObservableCollection<Ship>(_dbContext.Ships);
             CruiseRecords = new ObservableCollection<Cruise>(_dbContext.Cruises);
+            RoomRecords = new ObservableCollection<Room>(_dbContext.Rooms);
+            PassengerRecords = new ObservableCollection<Passenger>(_dbContext.Passengers);
+            DestinationRecords = new ObservableCollection<Destination>(_dbContext.Destinations);
+            Cruise_DurationRecords = new ObservableCollection<Cruise_Duration>(_dbContext.Cruise_Duration);
         }  
     }
 }
