39bcf6d064e75719b54d3cc7ff7a02f4fa7ccf74
Kevork <kevork_t@abv.bg>
2019-11-07 01:46:12 +0200

    Добавена функционалност за изтриване  на данни както в DB tables,тъка и във всички RDB tables

--- CascadeCruises/ViewModels/MainViewModel.cs
+++ CascadeCruises/ViewModels/MainViewModel.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 
 namespace CascadeCruises.ViewModels
 {
-   public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : INotifyPropertyChanged
     {
         private Cascade_CruisesEntities _dbContext;
 
@@ -29,6 +29,12 @@ namespace CascadeCruises.ViewModels
         public DelegateCommand DestinationSeachCommand { get; set; }
         public DelegateCommand CruiseDurationSearchCommand { get; set; }
         public DelegateCommand CruiseSearchCommand { get; set; }
+        public DelegateCommand ShipRemoveCommand { get; set; }
+        public DelegateCommand PassengerRemoveCommand { get; set; }
+        public DelegateCommand RoomRemoveCommand { get; set; }
+        public DelegateCommand DestinationRemoveCommand { get; set; }
+        public DelegateCommand CruiseDurationRemoveCommand { get; set; }
+        public DelegateCommand CruiseRemoveCommand { get; set; }
 
         private Ship _searchedShip;
         private Room _searchedRoom;
@@ -36,14 +42,14 @@ namespace CascadeCruises.ViewModels
         private Destination _searchedDestination;
         private Cruise_Duration _searchedCruiseDuration;
         private Cruise _seachedCruise;
-        public Ship SearchedShip 
+        public Ship SearchedShip
         {
-            get =>  _searchedShip;
+            get => _searchedShip;
             set
             {
                 _searchedShip = value;
                 INotifyChanged(nameof(SearchedShip));
-            } 
+            }
         }
         public Room SearchedRoom
         {
@@ -90,8 +96,6 @@ namespace CascadeCruises.ViewModels
                 INotifyChanged(nameof(SearchedCruise));
             }
         }
-
-
         public MainViewModel()
         {
             _dbContext = new Cascade_CruisesEntities();
@@ -101,14 +105,24 @@ namespace CascadeCruises.ViewModels
             PassengerRecords = new ObservableCollection<Passenger>(_dbContext.Passengers);
             DestinationRecords = new ObservableCollection<Destination>(_dbContext.Destinations);
             Cruise_DurationRecords = new ObservableCollection<Cruise_Duration>(_dbContext.Cruise_Duration);
+            InitializeCommands();
+        }
+        private void InitializeCommands()
+        {
             ShipSearchCommand = new DelegateCommand(ShipSearch);
             RoomSearchCommand = new DelegateCommand(RoomSearch);
             PassengerSearchCommand = new DelegateCommand(PassengerSearch);
             DestinationSeachCommand = new DelegateCommand(DestinationSearch);
             CruiseDurationSearchCommand = new DelegateCommand(CruiseDurationSearch);
             CruiseSearchCommand = new DelegateCommand(CruiseSearch);
-        }  
-        public void INotifyChanged (string name)
+            ShipRemoveCommand = new DelegateCommand(ShipRemove);
+            PassengerRemoveCommand = new DelegateCommand(PassengerRemove);
+            RoomRemoveCommand = new DelegateCommand(RoomRemove);
+            DestinationRemoveCommand = new DelegateCommand(DestinationRemove);
+            CruiseDurationRemoveCommand = new DelegateCommand(CruiseDurationRemove);
+            CruiseRemoveCommand = new DelegateCommand(CruiseRemove);
+        }
+        public void INotifyChanged(string name)
         {
             PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
         }
@@ -116,7 +130,7 @@ namespace CascadeCruises.ViewModels
         public void ShipSearch(object sender)
         {
             string text = (string)sender;
-            if(int.TryParse(text,out int id))
+            if (int.TryParse(text, out int id))
             {
                 SearchedShip = ShipRecords.FirstOrDefault(s => s.ID == id);
             }
@@ -183,6 +197,102 @@ namespace CascadeCruises.ViewModels
             else
             {
                 MessageBox.Show("Invalid ID");
+            }
+        }
+        public void ShipRemove(object sender)
+        {
+            if(SearchedShip!=null)
+            {
+                foreach (var room in SearchedShip.Rooms)
+                {
+                    foreach (var passenger in room.Passengers)
+                    {
+                        PassengerRecords.Remove(passenger);
+                    }
+                    _dbContext.Passengers.RemoveRange(room.Passengers);
+                    RoomRecords.Remove(room);
+                }
+                    _dbContext.Rooms.RemoveRange(SearchedShip.Rooms);
+
+                foreach(var ship in SearchedShip.Cruises)
+                {
+                    CruiseRecords.Remove(ship);
+                }
+                _dbContext.Cruises.RemoveRange(SearchedShip.Cruises);
+                _dbContext.Ships.Remove(SearchedShip);
+                ShipRecords.Remove(SearchedShip);
+                SearchedShip = null;
+                _dbContext.SaveChanges();
+            }
+        }
+        public void PassengerRemove(object sender)
+        {
+            if(SearchedPassenger!=null)
+            {
+                _dbContext.Passengers.Remove(SearchedPassenger);
+                PassengerRecords.Remove(SearchedPassenger);
+                SearchedPassenger = null;
+                _dbContext.SaveChanges();
+            }      
+        }
+        public void RoomRemove(object sender)
+        {
+            if(SearchedRoom!=null)
+            {
+                foreach (var passenger in SearchedRoom.Passengers)
+                {
+                    PassengerRecords.Remove(passenger);        
+                }
+                _dbContext.Passengers.RemoveRange(SearchedRoom.Passengers);
+                _dbContext.Rooms.Remove(SearchedRoom);
+                RoomRecords.Remove(SearchedRoom);
+                SearchedRoom = null;
+                _dbContext.SaveChanges();
+            }
+        }
+        public void DestinationRemove(object sender)
+        {
+            if (SearchedDestination != null)
+            {
+                foreach(var cruise in SearchedDestination.Cruises)
+                {
+                    CruiseRecords.Remove(cruise);
+                }
+                foreach(var cruise in SearchedDestination.Cruises1)
+                {
+                    CruiseRecords.Remove(cruise);
+                }
+                _dbContext.Cruises.RemoveRange(SearchedDestination.Cruises);
+                _dbContext.Cruises.RemoveRange(SearchedDestination.Cruises1);
+                _dbContext.Destinations.Remove(SearchedDestination);
+                DestinationRecords.Remove(SearchedDestination);
+                SearchedDestination = null;
+                _dbContext.SaveChanges();
+            }
+        }
+        public void CruiseDurationRemove(object sender)
+        {
+            if (SearchedCruiseDuration != null)
+            {
+                foreach(var cruise in SearchedCruiseDuration.Cruises)
+                {
+                    CruiseRecords.Remove(cruise);
+                }
+                _dbContext.Cruises.RemoveRange(SearchedCruiseDuration.Cruises);
+                _dbContext.Cruise_Duration.Remove(SearchedCruiseDuration);
+                Cruise_DurationRecords.Remove(SearchedCruiseDuration);
+                SearchedCruiseDuration = null;
+                _dbContext.SaveChanges();
+            }
+        }
+        public void CruiseRemove(object sender)
+        {
+            if (SearchedCruise != null)
+            {
+                _dbContext.Cruises.Remove(SearchedCruise);
+                CruiseRecords.Remove(SearchedCruise);
+                SearchedCruise = null;
+                _dbContext.SaveChanges();
             }
         }
     }
