c9151981a36cbf2de5c2af37ad8000e1c2ef7ab2
Kevork <kevork_t@abv.bg>
2019-11-06 05:00:50 +0200

    Добавена функционалност за тарсене на кораби по ид

--- CascadeCruises/ViewModels/MainViewModel.cs
+++ CascadeCruises/ViewModels/MainViewModel.cs
@@ -1,23 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace CascadeCruises.ViewModels
 {
-   public class MainViewModel
+   public class MainViewModel : INotifyPropertyChanged
     {
         private Cascade_CruisesEntities _dbContext;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public ObservableCollection<Ship> ShipRecords { get; set; }
         public ObservableCollection<Cruise> CruiseRecords { get; set; }
         public ObservableCollection<Room> RoomRecords { get; set; }
         public ObservableCollection<Passenger> PassengerRecords { get; set; }
         public ObservableCollection<Destination> DestinationRecords { get; set; }
         public ObservableCollection<Cruise_Duration> Cruise_DurationRecords { get; set; }
+        public DelegateCommand ShipSearchCommand { get; set; }
+        private Ship _searchedShip;
+        public Ship SearchedShip 
+        {
+            get =>  _searchedShip;
+            set
+            {
+                _searchedShip = value;
+                INotifyChanged(nameof(SearchedShip));
+            } 
+        }
+
         public MainViewModel()
         {
             _dbContext = new Cascade_CruisesEntities();
@@ -27,6 +44,24 @@ namespace CascadeCruises.ViewModels
             PassengerRecords = new ObservableCollection<Passenger>(_dbContext.Passengers);
             DestinationRecords = new ObservableCollection<Destination>(_dbContext.Destinations);
             Cruise_DurationRecords = new ObservableCollection<Cruise_Duration>(_dbContext.Cruise_Duration);
+            ShipSearchCommand = new DelegateCommand(ShipSearch);
         }  
+        public void INotifyChanged (string name)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void ShipSearch(object sender)
+        {
+            string text = (string)sender;
+            if(int.TryParse(text,out int id))
+            {
+                SearchedShip = ShipRecords.FirstOrDefault(s => s.ID == id);
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID");
+            }
+        }
     }
 }
